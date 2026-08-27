5eff902da7285e90908472885db44a1651527273
safi-erp-user <meyson672@gmail.com>
2026-08-28 02:00:51 +0330

    Update utils/persian_text.py

--- utils/persian_text.py
+++ utils/persian_text.py
@@ -1,22 +1,26 @@
 import arabic_reshaper
 from bidi.algorithm import get_display
+import re
 
 
 def reshape_text(text):
-    """تبدیل متن فارسی به فرمت صحیح برای نمایش"""
+    """تبدیل متن فارسی برای نمایش صحیح در Kivy"""
     if not text:
         return ''
 
     if isinstance(text, str):
         try:
-            reshaped = arabic_reshaper.reshape(text)
-            bidi_text = get_display(reshaped)
-            return bidi_text
+            parts = re.split(r'(\d+|[a-zA-Z]+)', text)
+            result = []
+            for part in parts:
+                if part:
+                    if re.match(r'^[\u0600-\u06FF\s]+$', part):
+                        reshaped = arabic_reshaper.reshape(part)
+                        bidi = get_display(reshaped)
+                        result.append(bidi)
+                    else:
+                        result.append(part)
+            return ''.join(result)
         except:
             return text
     return str(text)
-
-
-def rtl_text(text):
-    """تبدیل متن فارسی برای نمایش RTL"""
-    return reshape_text(text)
